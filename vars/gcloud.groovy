ac5b2323763bef34056e2a8f10106586f696c7b6
June Rhodes <jrhodes@redpoint.games>
2018-04-23 22:22:58 +1000

    Set up credentials to work with gsutil as well

--- vars/gcloud.groovy
+++ vars/gcloud.groovy
@@ -2,15 +2,21 @@ def call(java.util.LinkedHashMap config, org.jenkinsci.plugins.workflow.cps.CpsC
   def cwd = pwd()
   sh 'mkdir .gcloud'
   try {
-    withEnv(['GCLOUD_CONFIG=' + cwd + '/.gcloud']) {
+    withEnv(['GCLOUD_CONFIG=' + cwd + '/.gcloud', 'BOTO_CONFIG=' + cwd + '/.gcloud/boto.cfg']) {
       if (config["serviceAccountCredential"] != null) {
         withCredentials([file(credentialsId: config["serviceAccountCredential"], variable: 'GOOGLE_APPLICATION_CREDENTIALS')]) {
-          sh ('gcloud auth activate-service-account --key-file="$GOOGLE_APPLICATION_CREDENTIALS"')
+          sh 'gcloud config set pass_credentials_to_gsutil false'
+          sh 'echo "$GOOGLE_APPLICATION_CREDENTIALS" | gsutil config -e -o "$(pwd)/.gcloud/boto.cfg"'
+          sh 'gcloud auth activate-service-account --key-file="$GOOGLE_APPLICATION_CREDENTIALS"'
           block()
         }
       } else if (config["serviceAccountPath"] != null) {
-        sh ('gcloud auth activate-service-account --key-file="' + config["serviceAccountPath"] + '"')
-        block()
+        withEnv(['GOOGLE_APPLICATION_CREDENTIALS=' + config["serviceAccountPath"]]) {
+          sh 'gcloud config set pass_credentials_to_gsutil false'
+          sh 'echo "$GOOGLE_APPLICATION_CREDENTIALS" | gsutil config -e -o "$(pwd)/.gcloud/boto.cfg"'
+          sh 'gcloud auth activate-service-account --key-file="$GOOGLE_APPLICATION_CREDENTIALS"'
+          block()
+        }
       } else {
         // No config options set.
         block()
