a26303290a83b63c97cd38ec8a5aea6741d40ecf
June Rhodes <jrhodes@redpoint.games>
2018-04-23 21:56:53 +1000

    Try this

--- vars/gcloud.groovy
+++ vars/gcloud.groovy
@@ -1,15 +1,15 @@
-def call(Closure block, String serviceAccountPath = null, String serviceAccountCredential = null) {
+def call(java.util.LinkedHashMap config, org.jenkinsci.plugins.workflow.cps.CpsClosure2 block) {
   def cwd = pwd()
   sh 'mkdir .gcloud'
   try {
     withEnv(['GCLOUD_CONFIG=' + cwd + '/.gcloud']) {
-      if (serviceAccountCredential != null) {
-        withCredentials([file(credentialsId: serviceAccountCredential, variable: 'GOOGLE_APPLICATION_CREDENTIALS')]) {
+      if (config["serviceAccountCredential"] != null) {
+        withCredentials([file(credentialsId: config["serviceAccountCredential"], variable: 'GOOGLE_APPLICATION_CREDENTIALS')]) {
           sh ('gcloud auth activate-service-account --key-file="$GOOGLE_APPLICATION_CREDENTIALS"')
           block()
         }
-      } else if (serviceAccountPath != null) {
-        sh ('gcloud auth activate-service-account --key-file="' + serviceAccountPath + '"')
+      } else if (config["serviceAccountPath"] != null) {
+        sh ('gcloud auth activate-service-account --key-file="' + config["serviceAccountPath"] + '"')
         block()
       } else {
         // No config options set.
