7f43cbb1e7a21a3107d55e2e53bfe5b29188bb5e
June Rhodes <jrhodes@redpoint.games>
2018-04-23 22:33:44 +1000

    Use full paths to GCLOUD_CONFIG directory

--- vars/gcloud.groovy
+++ vars/gcloud.groovy
@@ -7,18 +7,18 @@ def call(java.util.LinkedHashMap config, org.jenkinsci.plugins.workflow.cps.CpsC
       // get deleted once withCredentials goes out of scope).
       if (config["serviceAccountCredential"] != null) {
         withCredentials([file(credentialsId: config["serviceAccountCredential"], variable: 'GOOGLE_APPLICATION_CREDENTIALS')]) {
-          sh 'cp $GOOGLE_APPLICATION_CREDENTIALS .gcloud/serviceaccount.json'
+          sh 'cp "$GOOGLE_APPLICATION_CREDENTIALS" "$GCLOUD_CONFIG/serviceaccount.json"'
         }
       } else if (config["serviceAccountPath"] != null) {
         withEnv(['GOOGLE_APPLICATION_CREDENTIALS=' + config["serviceAccountPath"]]) {
-          sh 'cp $GOOGLE_APPLICATION_CREDENTIALS .gcloud/serviceaccount.json'
+          sh 'cp "$GOOGLE_APPLICATION_CREDENTIALS" "$GCLOUD_CONFIG/serviceaccount.json"'
         }
       }
 
       // Set up service account authentication in the temporary Google Cloud config path.
       sh 'gcloud config set pass_credentials_to_gsutil false'
-      sh 'echo "$(pwd)/.gcloud/serviceaccount.json" | gsutil config -e -o "$(pwd)/.gcloud/boto.cfg"'
-      sh 'gcloud auth activate-service-account --key-file="$(pwd)/.gcloud/serviceaccount.json"'
+      sh 'echo "$GCLOUD_CONFIG/serviceaccount.json" | gsutil config -e -o "$GCLOUD_CONFIG/boto.cfg"'
+      sh 'gcloud auth activate-service-account --key-file="$GCLOUD_CONFIG/serviceaccount.json"'
 
       // Invoke the closure block that the user wants to execute, with the GCLOUD_CONFIG
       // and BOTO_CONFIG environment variables set.
