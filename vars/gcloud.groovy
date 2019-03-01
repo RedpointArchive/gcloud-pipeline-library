360e00dc9fdd2c6a7b383f58a8a955af4e3ded57
June Rhodes <jrhodes@redpoint.games>
2019-03-01 14:16:59 -0800

    Include app default credentials and authorize Docker for gcr.io

--- vars/gcloud.groovy
+++ vars/gcloud.groovy
@@ -2,26 +2,32 @@ def call(java.util.LinkedHashMap config, org.jenkinsci.plugins.workflow.cps.CpsC
   def cwd = pwd()
   sh 'mkdir .gcloud'
   try {
-    withEnv(['CLOUDSDK_CONFIG=' + cwd + '/.gcloud', 'BOTO_CONFIG=' + cwd + '/.gcloud/boto.cfg']) {
+    withEnv([
+      'CLOUDSDK_CONFIG=' + cwd + '/.gcloud', 
+      'BOTO_CONFIG=' + cwd + '/.gcloud/boto.cfg', 
+      'GOOGLE_APPLICATION_CREDENTIALS=' + cwd + '/.gcloud/serviceaccount.json',
+      'DOCKER_CONFIG=' + cwd + '/.gcloud/docker.config'
+    ]) {
       // Copy the service account JSON file to our Google Cloud config directory (so it doesn't
       // get deleted once withCredentials goes out of scope).
       if (config["serviceAccountCredential"] != null) {
-        withCredentials([file(credentialsId: config["serviceAccountCredential"], variable: 'GOOGLE_APPLICATION_CREDENTIALS')]) {
-          sh 'cp "$GOOGLE_APPLICATION_CREDENTIALS" "$CLOUDSDK_CONFIG/serviceaccount.json"'
+        withCredentials([file(credentialsId: config["serviceAccountCredential"], variable: 'GOOGLE_APPLICATION_CREDENTIALS_TMP')]) {
+          sh 'cp "$GOOGLE_APPLICATION_CREDENTIALS_TMP" "$GOOGLE_APPLICATION_CREDENTIALS"'
         }
       } else if (config["serviceAccountPath"] != null) {
-        withEnv(['GOOGLE_APPLICATION_CREDENTIALS=' + config["serviceAccountPath"]]) {
-          sh 'cp "$GOOGLE_APPLICATION_CREDENTIALS" "$CLOUDSDK_CONFIG/serviceaccount.json"'
+        withEnv(['GOOGLE_APPLICATION_CREDENTIALS_TMP=' + config["serviceAccountPath"]]) {
+          sh 'cp "$GOOGLE_APPLICATION_CREDENTIALS_TMP" "$GOOGLE_APPLICATION_CREDENTIALS"'
         }
       }
 
       // Set up service account authentication in the temporary Google Cloud config path.
       sh 'gcloud config set pass_credentials_to_gsutil false'
-      sh 'echo "$CLOUDSDK_CONFIG/serviceaccount.json" | gsutil config -e -o "$CLOUDSDK_CONFIG/boto.cfg"'
-      sh 'gcloud auth activate-service-account --key-file="$CLOUDSDK_CONFIG/serviceaccount.json"'
+      sh 'echo "$GOOGLE_APPLICATION_CREDENTIALS" | gsutil config -e -o "$CLOUDSDK_CONFIG/boto.cfg"'
+      sh 'gcloud auth activate-service-account --key-file="$GOOGLE_APPLICATION_CREDENTIALS"'
+      sh 'gcloud auth print-access-token | docker login -u oauth2accesstoken --password-stdin https://gcr.io'
 
-      // Invoke the closure block that the user wants to execute, with the CLOUDSDK_CONFIG
-      // and BOTO_CONFIG environment variables set.
+      // Invoke the closure block that the user wants to execute, with all the appropriate
+      // environment variables set.
       block()
     }
   } finally {
